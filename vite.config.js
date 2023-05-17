61cea3c76499f39071b2e9912f223c1e19bf99a7
교보문고 <kyobobook@gyobomungoui-MacBookPro.local>
2023-05-17 20:28:37 +0900

    0517

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tictactoe-t2'
+  // 깃헙 저장소 이름
+  base: isProd ? '/tictactoe-t2' : '/'
 })
